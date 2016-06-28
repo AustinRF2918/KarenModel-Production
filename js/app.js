76488e4422adcfc5d343a61e0e024166116c9be8
Austin Fell <austinrf2918@yahoo.com>
2016-06-28 16:17:38 -0400

    Refactored javascript to be more reusable and maintainable

--- js/app.js
+++ js/app.js
@@ -1,34 +1,36 @@
-var page_initialization = function()
-{
-    $(".slide-out").addClass("slide-out-toggleOff");
-    console.log("Page initialized.");
-};
-
+//Toggle construct.
 var switch_toggle = function()
 {
     if ($(".slide-out").hasClass("slide-out-toggleOff"))
-    {
 	$(".slide-out").removeClass("slide-out-toggleOff", 1000, "easeOutBounce");
-    }
     else
-    {
 	$(".slide-out").addClass("slide-out-toggleOff", 1000, "easeOutBounce");
-    }
 };
 
+//Simple DOM animation.
+//Attr this should be a a this value called from an
+//anonymous function( $('...').click(function(){ scroll_down(this...)})
+//Offset value designates to what point we should scroll past or before
+//our point, and time is obvious.
+var scroll_down = function(attr_this, offset_value, time)
+{
+    $('html, body').animate({
+	    scrollTop: $($.attr(attr_this, 'href')).offset().top - offset_value}, time);
+};
 
 $(document).ready(function(){
-    page_initialization();
-
+    //Functions as a switch (Could be done using CSS but
+    //JavaScript is easier. Switch toggle checks for
+    //presence of class or non-presense.
     $(".navbar-mobile-toggler").click(function(){
-	console.log("Switching toggle.");
 	switch_toggle();
     });
 
+    //If you click a link, we want it to scroll
+    //downwards instead of just going immediately to the link.
     $('a.slide-down').click(function(){
-	$('html, body').animate({
-	    scrollTop: $($.attr(this, 'href')).offset().top - 70}, 500);
-	    return false;
+	scroll_down(this, 70, 500);
+	return false;
     });
 });
 
